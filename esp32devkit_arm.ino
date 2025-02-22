7c0ba5d16de8a30ed2cd7c96ee62112e500c85f8
KateSt <89999131+KateySt@users.noreply.github.com>
2025-02-22 15:36:06 +0200

    Update esp32devkit_arm.ino

--- esp32devkit_arm.ino
+++ esp32devkit_arm.ino
@@ -14,6 +14,11 @@ Servo SERVO_B;
 #define SERVO_LR_PIN 33
 #define SERVO_B_PIN 25
 
+#define MOTOR_1_PIN_1 14
+#define MOTOR_1_PIN_2 32
+#define MOTOR_2_PIN_1 34
+#define MOTOR_2_PIN_2 35
+
 int SERVO_P_POS = 0;
 int SERVO_M_POS = 0;
 int SERVO_LR_POS = 0;
@@ -66,11 +71,30 @@ const char index_html[] PROGMEM = R"rawliteral(
             min-height: 100%;
             width: 100%;
         }
-
+      table { margin-left: auto; margin-right: auto; }
+      td { padding: 8 px; }
+      .button {
+        background-color: #2f4468;
+        border: none;
+        color: white;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 18px;
+        margin: 6px 3px;
+        cursor: pointer;
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        -webkit-tap-highlight-color: rgba(0,0,0,0);
+      }
         button,
         input {
             width: 25vw;
-            /* height: 10vh; */
             width: 100%;
         }
 
@@ -99,9 +123,6 @@ const char index_html[] PROGMEM = R"rawliteral(
 
     <div class="main">
         <div class="row">
-            <h3>Robotic Hand V1</h3>
-        </div>
-        <div class="row">
             <label for="">PALM</label>
             <input type="range" min="0" max="180" class="slider" onchange="movement('P',this.value)" />
         </div>
@@ -117,9 +138,19 @@ const char index_html[] PROGMEM = R"rawliteral(
             <label for="">BASE</label>
             <input type="range" min="0" max="180" class="slider" onchange="movement('B',this.value)" />
         </div>
+    <table>
+      <tr><td colspan="3" align="center"><button class="button" onmousedown="toggleCheckbox('forward');" ontouchstart="toggleCheckbox('forward');" onmouseup="toggleCheckbox('stop');" ontouchend="toggleCheckbox('stop');">Forward</button></td></tr>
+      <tr><td align="center"><button class="button" onmousedown="toggleCheckbox('left');" ontouchstart="toggleCheckbox('left');" onmouseup="toggleCheckbox('stop');" ontouchend="toggleCheckbox('stop');">Left</button></td><td align="center"><button class="button" onmousedown="toggleCheckbox('stop');" ontouchstart="toggleCheckbox('stop');">Stop</button></td><td align="center"><button class="button" onmousedown="toggleCheckbox('right');" ontouchstart="toggleCheckbox('right');" onmouseup="toggleCheckbox('stop');" ontouchend="toggleCheckbox('stop');">Right</button></td></tr>
+      <tr><td colspan="3" align="center"><button class="button" onmousedown="toggleCheckbox('backward');" ontouchstart="toggleCheckbox('backward');" onmouseup="toggleCheckbox('stop');" ontouchend="toggleCheckbox('stop');">Backward</button></td></tr>                   
+    </table>
     </div>
 
     <script>
+       function toggleCheckbox(x) {
+     var xhr = new XMLHttpRequest();
+     xhr.open("GET", "/action?go=" + x, true);
+     xhr.send();
+   }
         function movement(joint,directions) {
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
@@ -144,6 +175,11 @@ void setup(){
   SERVO_LR.attach(SERVO_LR_PIN);
   SERVO_B.attach(SERVO_B_PIN);
 
+  pinMode(MOTOR_1_PIN_1, OUTPUT);
+  pinMode(MOTOR_1_PIN_2, OUTPUT);
+  pinMode(MOTOR_2_PIN_1, OUTPUT);
+  pinMode(MOTOR_2_PIN_2, OUTPUT);
+
   Serial.begin(115200);
 
   servo_balance(SERVO_P, SERVO_P_POS);
@@ -156,18 +192,17 @@ void setup(){
     return;
   }
 
-  // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
-
-  Serial.println(WiFi.localIP());
   
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html);
-  });
+  Serial.println(WiFi.localIP());
+
+server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+  request->send_P(200, "text/html", index_html);
+});
 
 server.on("/move", HTTP_GET, [] (AsyncWebServerRequest *request) {
   String joint;
@@ -181,6 +216,8 @@ server.on("/move", HTTP_GET, [] (AsyncWebServerRequest *request) {
     Serial.print(joint);
     Serial.print(" & ");
     Serial.println(directions);
+    
+    //int move_state = state.toInt();
 
     if(joint.equals("P")) { servo_control(SERVO_P, directions_); }
     if(joint.equals("M")) { servo_control(SERVO_M, directions_); }
@@ -192,6 +229,52 @@ server.on("/move", HTTP_GET, [] (AsyncWebServerRequest *request) {
   
 });
 
+server.on("/action", HTTP_GET, [] (AsyncWebServerRequest *request) {
+  String go;
+  if (request->hasParam("go")) {
+    go = request->getParam("go")->value();
+
+    Serial.print(go);
+  if(go.equals("forward")) {
+    Serial.println("Forward");
+    digitalWrite(MOTOR_1_PIN_1, 1);
+    digitalWrite(MOTOR_1_PIN_2, 0);
+    digitalWrite(MOTOR_2_PIN_1, 1);
+    digitalWrite(MOTOR_2_PIN_2, 0);
+  }
+  else if(go.equals("left")) {
+    Serial.println("Left");
+    digitalWrite(MOTOR_1_PIN_1, 0);
+    digitalWrite(MOTOR_1_PIN_2, 1);
+    digitalWrite(MOTOR_2_PIN_1, 1);
+    digitalWrite(MOTOR_2_PIN_2, 0);
+  }
+  else if(go.equals("right")) {
+    Serial.println("Right");
+    digitalWrite(MOTOR_1_PIN_1, 1);
+    digitalWrite(MOTOR_1_PIN_2, 0);
+    digitalWrite(MOTOR_2_PIN_1, 0);
+    digitalWrite(MOTOR_2_PIN_2, 1);
+  }
+  else if(go.equals("backward")) {
+    Serial.println("Backward");
+    digitalWrite(MOTOR_1_PIN_1, 0);
+    digitalWrite(MOTOR_1_PIN_2, 1);
+    digitalWrite(MOTOR_2_PIN_1, 0);
+    digitalWrite(MOTOR_2_PIN_2, 1);
+  }
+  else if(!strcmp(variable, "stop")) {
+    Serial.println("Stop");
+    digitalWrite(MOTOR_1_PIN_1, 0);
+    digitalWrite(MOTOR_1_PIN_2, 0);
+    digitalWrite(MOTOR_2_PIN_1, 0);
+    digitalWrite(MOTOR_2_PIN_2, 0);
+  }
+    
+  }
+  request->send(200, "text/plain", "OK");
+  
+});
   server.begin();
 }
 
